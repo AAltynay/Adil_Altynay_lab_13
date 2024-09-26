c4b22c81c9acac9703a8f96cb0f408c850f69000
Adil Altynay <AlZAdil@beeline.kz>
2024-09-26 16:06:36 +0500

    Обновление изменений app.js

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const currencyToSelect = document.getElementById('currency-to');
 const convertBtn = document.getElementById('convert-btn');
 const conversionResult = document.getElementById('conversion-result');
 
-const apiKey = '1c22f42743c53799e73ddde8'; // Замените на ваш API ключ
+const apiKey = '1c22f42743c53799e73ddde8'; 
+let conversionRates = {}; 
 
 async function fetchExchangeRates() {
     try {
@@ -12,14 +13,24 @@ async function fetchExchangeRates() {
         if (!response.ok) throw new Error('Ошибка при получении данных');
 
         const data = await response.json();
+        if (data.result === "error") {
+            throw new Error(`Ошибка: ${data['error-type']}`);
+        }
+        
         updateExchangeRates(data.conversion_rates);
     } catch (error) {
-        exchangeRatesElement.textContent = error.message;
+        exchangeRatesElement.textContent = error.message; 
+        console.error(error); 
     }
 }
 
 function updateExchangeRates(rates) {
+    conversionRates = rates; 
     exchangeRatesElement.innerHTML = `<p>1 KZT = ${rates.USD} USD</p><p>1 KZT = ${rates.EUR} EUR</p>`;
+    
+ 
+    currencyFromSelect.innerHTML = '';
+    currencyToSelect.innerHTML = '';
     
     for (const currency in rates) {
         const optionFrom = document.createElement('option');
@@ -41,11 +52,22 @@ convertBtn.addEventListener('click', () => {
     const amount = parseFloat(document.getElementById('amount').value);
     const fromCurrency = currencyFromSelect.value;
     const toCurrency = currencyToSelect.value;
-    const rate = parseFloat(currencyToSelect.options[currencyToSelect.selectedIndex].text);
+
+    
+    if (!conversionRates[fromCurrency] || !conversionRates[toCurrency]) {
+        conversionResult.textContent = 'Ошибка: выбранная валютная пара не найдена.';
+        return; 
+    }
 
     if (!isNaN(amount) && amount > 0) {
-        const result = (amount * rate).toFixed(2);
-        conversionResult.textContent = `${amount} ${fromCurrency} = ${result} ${toCurrency}`;
+        const fromRate = conversionRates[fromCurrency]; 
+        const toRate = conversionRates[toCurrency]; 
+        
+        if (fromRate && toRate) {
+            const rate = toRate / fromRate; 
+            const result = (amount * rate).toFixed(2);
+            conversionResult.textContent = `${amount} ${fromCurrency} = ${result} ${toCurrency}`;
+        }
     } else {
         conversionResult.textContent = 'Введите корректную сумму';
     }
